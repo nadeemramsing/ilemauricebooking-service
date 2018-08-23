32cc213ab1cc24b4e724d4b535e6d1eae3a42623
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-08-23 12:18:24 +0400

    Env. var. added to res.send (TESTING)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const
   app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to ilemauricebooking-service.');
+  res.send('Welcome to ilemauricebooking-service. My name is ' + process.env.NAME);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000! My name is ' + process.env.NAME));
