c7de5fd29c5f1247a903ef0846e3a5586839edbd
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-08-23 11:59:36 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const
+  express = require('express'),
+  app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ilemauricebooking-service.');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000! My name is ' + process.env.NAME));
