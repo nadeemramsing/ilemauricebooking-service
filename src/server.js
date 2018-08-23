788b3092e94f6ba2fc565fe006aff695012b953a
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-08-23 14:55:33 +0400

    Code refactor

--- src/server.js
+++ src/server.js
@@ -8,4 +8,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to ilemauricebooking-service.');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+const port = process.env.PORT || 4001;
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
