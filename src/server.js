986182d84cbe63bf954cbbc07cb440cd5f702e5a
Nadeem Ramsing <nadeem.ramsing@gmail.com>
2018-09-04 15:54:08 +0400

    Code refactor: "main" flow control used in server.js (entrypoint of service)

--- src/server.js
+++ src/server.js
@@ -1,34 +1,10 @@
 require('dotenv').config();
 
-const
-  express = require('express'),
-  db = require('./mongodb-stitch-db'),
+const express = require('express');
+const stitchDB = require('./stitch-db');
+const app = express();
 
-  app = express();
-
-db
-  .connect()
-  .then(async connection => {
-    const propertiesCollection = connection.db.collection('properties');
-
-    const insertedProperty = await propertiesCollection
-      .insertOne({
-        name: 'Nadeem Ramsing',
-        owner_id: connection.ownerId
-      })
-      .catch(err => console.log(err));
-
-    const properties = await propertiesCollection
-      .find()
-      .execute()
-      .catch(err => console.log(err));
-
-    const count = await propertiesCollection
-      .count()
-      .catch(err => console.log(err));
-
-    console.log(insertedProperty, properties, count);
-  });
+main();
 
 app.get('/', (req, res) => {
   res.send('Welcome to ilemauricebooking-service.');
@@ -37,3 +13,9 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+// MAIN
+async function main() {
+  const connection = await stitchDB.connect()
+  stitchDB.test();
+}
--- src/stitch-db.js
+++ src/stitch-db.js
@@ -28,6 +28,29 @@ async function connect() {
   }
 }
 
+async function test() {
+  const propertiesCollection = connection.db.collection('properties');
+
+  const insertedProperty = await propertiesCollection
+    .insertOne({
+      name: 'Nadeem Ramsing',
+      owner_id: connection.ownerId
+    })
+    .catch(err => console.log(err));
+
+  const properties = await propertiesCollection
+    .find()
+    .execute()
+    .catch(err => console.log(err));
+
+  const count = await propertiesCollection
+    .count()
+    .catch(err => console.log(err));
+
+  console.log(insertedProperty, properties, count);
+}
+
 module.exports = {
-  connect
+  connect,
+  test
 }
